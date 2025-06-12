fd7418a98cb2e683038efddb901e06a282f9c9fa
João Victor Seide <seidejvs@gmail.com>
2025-06-12 18:29:29 -0300

    criei o projeto em Spring

---
+++ TCC_Cedup_DS302/src/test/java/cedup/tcc/TCC_Cedup_DS302/TccCedupDs302ApplicationTests.java
@@ -0,0 +1,13 @@
+package cedup.tcc.TCC_Cedup_DS302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccCedupDs302ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
