ecb074eb864e6221f1250a7cb163d270be12e85f
seidejoao <seidejvs@gmail.com>
2025-06-12 15:54:57 -0300

    criei o projeto em spring com Java

---
+++ TCC_Cedup_DS302/TCC_Cedup_DS302/src/main/java/tcc/cedup/TCC_Cedup_DS302/TccCedupDs302Application.java
@@ -0,0 +1,13 @@
+package tcc.cedup.TCC_Cedup_DS302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccCedupDs302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccCedupDs302Application.class, args);
+	}
+
+}
---
+++ TCC_Cedup_DS302/TCC_Cedup_DS302/src/test/java/tcc/cedup/TCC_Cedup_DS302/TccCedupDs302ApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.cedup.TCC_Cedup_DS302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccCedupDs302ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
