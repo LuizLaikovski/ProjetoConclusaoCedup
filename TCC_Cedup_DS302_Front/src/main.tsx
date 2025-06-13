e57d06ccdc3f81c294721b19a30efa32f4590978
Luiz Laikovski <luizlaikovski@gmail.com>
2025-06-12 21:47:37 -0300

    Iniciando o Front

--- TCC_Cedup_DS302_Front/src/main.tsx
+++ TCC_Cedup_DS302_Front/src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Home from './routes/Home.tsx';
+import Login from './routes/Login.tsx';
+import ErrorPage from './routes/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "login",
+        element: <Login />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
