35679dccabb040a48ab40744a3220bb56b397577
Luiz Laikovski <luizlaikovski@gmail.com>
2025-06-28 16:27:51 -0300

    header pronto

---
+++ TCC_Cedup_DS302_Front/src/components/Header.tsx
@@ -0,0 +1,41 @@
+import './css/header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+  return (
+    <header className='header'>
+      <div className='align-header'>
+        <img src="" alt="" />
+
+        <form className="search-container">
+          <input
+            type="text"
+            placeholder="Digite sua busca..."
+            className="search-input"
+          />
+          <button type="submit" className="search-button">
+            <FontAwesomeIcon icon={faSearch} color='black' size='2x' />
+          </button>
+        </form>
+
+        <Link to="/login" className='link-to-login-header'>Faça seu Login</Link>
+        <button className='button-for-favorites-header'>
+          <FontAwesomeIcon icon={faHeart} size='2x' className='favorite-book-header'/>
+        </button>
+      </div>
+      <div className='container-category-header'>
+        <ul>     {/* Futuramente implemenatr botões no lugar dos li's para querys */}
+          <li>Todos</li>     
+          <li>Mais Procurados</li>
+          <li>Em Alta</li>
+          <li>Clássicos</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- TCC_Cedup_DS302_Front/src/routes/Home.tsx
+++ TCC_Cedup_DS302_Front/src/routes/Home.tsx
@@ -1,9 +1,11 @@
 import RouteButton from "../components/RouteButton";
 import "../App.css";
+import Header from "../components/Header";
 
 const Home = () => {
     return(
         <>
+            <Header  />
             <h1>HOME</h1>
             <RouteButton label="LOGIN" path="/login" />
             <RouteButton label="catalogo" path="/home/catalogo" />
