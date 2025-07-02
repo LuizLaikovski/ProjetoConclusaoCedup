b86946674acbf7af4b20bdd9564989e5d05a1f62
João Victor Seide <seidejvs@gmail.com>
2025-07-02 16:16:13 -0300

    deletei tudo danise se precisar volta atras e bença

--- TCC_Cedup_DS302/src/main/java/cedup/tcc/TCC_Cedup_DS302/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package cedup.tcc.TCC_Cedup_DS302.repository;
-
-import cedup.tcc.TCC_Cedup_DS302.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
--- TCC_Cedup_DS302/src/main/java/cedup/tcc/TCC_Cedup_DS302/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package cedup.tcc.TCC_Cedup_DS302.repository;
-
-import cedup.tcc.TCC_Cedup_DS302.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
