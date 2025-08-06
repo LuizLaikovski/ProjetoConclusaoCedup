1ac176be2c8e9c52ce15e1d2220efe6b1df5dc32
João Victor Seide <seidejvs@gmail.com>
2025-08-06 14:40:58 -0300

    fiz cagada e arrumei
    principalmente alterei o banco de dados e mexi no diretório da aplicação

--- DB/DataBase_Project.sql
+++ DB/DataBase_Project.sql
@@ -1,33 +1,59 @@
-CREATE SCHEMA CYBRARY_TCC_DS302;
-USE CYBRARY_TCC_DS302;
+CREATE SCHEMA PROJETO_CONCLUSAO_DS302;
+USE PROJETO_CONCLUSAO_DS302;
 
-CREATE TABLE book (
-  id_book int NOT NULL AUTO_INCREMENT,
-  book_title varchar(100) NOT NULL,
-  book_author int NOT NULL,
-  book_genre varchar(60) DEFAULT NULL,
-  book_quant_pags int NOT NULL,
-  book_rating decimal(2,2) DEFAULT NULL,
-  book_image int NOT NULL,
-  PRIMARY KEY (id_book),
-  KEY book_id_author_idx (book_author),
-  KEY book_id_image_idx (book_image),
-  CONSTRAINT book_id_author FOREIGN KEY (book_author) REFERENCES author (id_author),
-  CONSTRAINT book_id_image FOREIGN KEY (book_image) REFERENCES image (id_image)
+CREATE TABLE `author` (
+  `id_author` int NOT NULL AUTO_INCREMENT,
+  `author_name` varchar(75) NOT NULL,
+  `author_year_born` date DEFAULT NULL,
+  `author_year_death` date DEFAULT NULL,
+  `author_about` text,
+  PRIMARY KEY (`id_author`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE author (
-  id_author int NOT NULL AUTO_INCREMENT,
-  author_name varchar(75) NOT NULL,
-  author_year_born date DEFAULT NULL,
-  author_year_death date DEFAULT NULL,
-  author_about text,
-  PRIMARY KEY (id_author)
+CREATE TABLE `book` (
+  `id_book` int NOT NULL AUTO_INCREMENT,
+  `book_title` varchar(100) NOT NULL,
+  `book_quant_pags` int NOT NULL,
+  `book_rating` decimal(2,2) DEFAULT NULL,
+  PRIMARY KEY (`id_book`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE image (
-  id_image int NOT NULL AUTO_INCREMENT,
-  image_src varchar(45) NOT NULL,
-  image_alt varchar(45) NOT NULL,
-  PRIMARY KEY (id_image)
+CREATE TABLE `genre` (
+  `id_genre` int NOT NULL AUTO_INCREMENT,
+  `genre_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `image` (
+  `id_image` int NOT NULL AUTO_INCREMENT,
+  `image_src` text NOT NULL,
+  `image_alt` text NOT NULL,
+  PRIMARY KEY (`id_image`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `book_author` (
+  `id_book` int NOT NULL,
+  `id_author` int NOT NULL,
+  PRIMARY KEY (`id_book`,`id_author`),
+  KEY `id_author_idx` (`id_author`),
+  CONSTRAINT `id_author_book` FOREIGN KEY (`id_author`) REFERENCES `author` (`id_author`),
+  CONSTRAINT `id_book_author` FOREIGN KEY (`id_book`) REFERENCES `book` (`id_book`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `book_genre` (
+  `id_book` int NOT NULL,
+  `id_genre` int NOT NULL,
+  PRIMARY KEY (`id_book`,`id_genre`),
+  KEY `id_genre_idx` (`id_genre`),
+  CONSTRAINT `id_book_genre` FOREIGN KEY (`id_book`) REFERENCES `book` (`id_book`),
+  CONSTRAINT `id_genre_book` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `book_image` (
+  `id_book` int NOT NULL,
+  `id_image` int NOT NULL,
+  PRIMARY KEY (`id_book`,`id_image`),
+  KEY `id_image_book_idx` (`id_image`),
+  CONSTRAINT `id_book_image` FOREIGN KEY (`id_book`) REFERENCES `book` (`id_book`),
+  CONSTRAINT `id_image_book` FOREIGN KEY (`id_image`) REFERENCES `image` (`id_image`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
