211d2e39ab7b1ad120995c7b70bba446fdd334e4
João Victor Seide <seidejvs@gmail.com>
2025-09-15 09:15:24 -0300

    Removi tudo para trocar de banco de dados
    vo trocar pro mongodb

--- project/src/main/java/project/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package project.repository;
-
-import project.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
