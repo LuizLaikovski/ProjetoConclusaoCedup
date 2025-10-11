7b44d960fc6929c548d7dc2f223080ec9fbe3f56
João Victor Seide <seidejvs@gmail.com>
2025-10-11 17:14:57 -0300

    base do projeto

---
+++ projetoconclusaocedup/src/main/java/com/projetoconclusaocedup/ProjetoConclusaoCedupApplication.java
@@ -0,0 +1,13 @@
+package com.projetoconclusaocedup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConclusaoCedupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConclusaoCedupApplication.class, args);
+	}
+
+}
---
+++ projetoconclusaocedup/src/test/java/com/projetoconclusaocedup/ProjetoConclusaoCedupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoconclusaocedup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConclusaoCedupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
