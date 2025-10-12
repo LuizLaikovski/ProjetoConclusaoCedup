4de67355cc67d9da65a70104e25090371165a3f7
Luiz Laikovski <luizlaikovski@gmail.com>
2025-10-12 11:29:01 -0300

    Arrumando para deploy

--- TCC_Cedup_DS302_Front/vite.config.ts
+++ TCC_Cedup_DS302_Front/vite.config.ts
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {sourcemap: true,},
   plugins: [react(), tailwindcss()],
-})
+  build: {outDir: 'dist',
+
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
